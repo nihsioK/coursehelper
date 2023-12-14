4bbdb429be3f8be1b3cb9bb2924d67c5d302e12b
Nihsiok <daniyar.koishin@nu.edu.kz>
2023-12-14 14:56:16 +0600

    modified:   coursehelper/Dockerfile 	modified:   coursehelper/coursehelper/settings.py 	modified:   coursehelper/requirements.txt

--- coursehelper/coursehelper/settings.py
+++ coursehelper/coursehelper/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['test2-bfyak76upq-uc.a.run.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
